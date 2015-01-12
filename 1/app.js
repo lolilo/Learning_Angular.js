3d5bf99ea79bd7f172683e38333897d307d42918
Liana Lo <liana.lo@prezi.com>
2015-01-12 13:46:13 +0100

    initial commit

---
+++ 1/app.js
@@ -0,0 +1,5 @@
+// 'store' = application name
+// [ ] = dependencies, other libraries needed. Still need to pass in empty
+// array even if we have no dependencies
+
+var app = angular.module('store', [ ]);
