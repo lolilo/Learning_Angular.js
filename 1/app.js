a47f60009f9bc70b08d6ffdbf8795e831398cef4
Liana Lo <liana.lo@prezi.com>
2015-01-12 14:38:29 +0100

    built-in directives

--- 1/app.js
+++ 1/app.js
@@ -2,4 +2,27 @@
 // [ ] = dependencies, other libraries needed. Still need to pass in empty
 // array even if we have no dependencies
 
-var app = angular.module('store', [ ]);
+(function() {
+    var app = angular.module('store', [ ]);
+    
+    app.controller('StoreController', function(){
+	this.products = gems; // product is a property of the controller
+	});
+
+    var gems = [
+        {
+    	name: 'Dodecahedron', 
+    	price: 2.95, 
+    	description: '. . .',
+    	canPurchase: true,
+    	soldOut: false,
+        },
+        {
+    	name: 'Pentagonal Gem', 
+    	price: 5.95, 
+    	description: '. . .',
+    	canPurchase: false,
+    	soldOut: false,
+        }
+    ];
+})();
